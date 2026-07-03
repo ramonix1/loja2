a3a23c721f5a4dea51fc5b7f43e92269b241ba2b
Barbosadev <silvag1212@gmail.com>
2026-07-03 02:08:52 -0300

    :bug: fix(infra): fix CI e2e smoke tests errors after refact UI

--- apps/e2e/fixtures/buyer.setup.ts
+++ apps/e2e/fixtures/buyer.setup.ts
@@ -1,4 +1,4 @@
-import { test as setup } from '@playwright/test';
+import { expect, test as setup } from '@playwright/test';
 import { testIds } from '@lojao/test-utils/test-ids';
 
 import { storePath } from '../lib/store-path';
